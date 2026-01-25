989e7bf96bfb720b4c9dfec2cbb71f0d007ad750
u0324 <tamago2605@gmail.com>
2026-01-26 01:03:44 +0900

    Update register.py

--- register.py
+++ register.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 # --- 設定 (環境変数を直接入れるか、os.getenvで取得) ---
-TOKEN = "MTQ0MDM3ODA2NjIyNzU2NDcyOQ.GO3iCX.mUVhnpdnI5Vt-xBL60X3pKhlYwMuXfU-mQJBss"  # Botのトークンを入れてください
+TOKEN = "MTQ0MDM3ODA2NjIyNzU2NDcyOQ.G4njp8.rjd8n10xRXn1IZnwm1nVSayjZdzoC73PwIg3MY"  # Botのトークンを入れてください
 APP_ID = "1440378066227564729"  # アプリケーションIDを入れてください
 GUILD_ID = "1421708178113953814"  # サーバーIDを入れると反映が爆速になります
 
